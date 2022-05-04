ea8335fbf212d4bcb53c494a63a0c9803fe5c8e7
KayodeOusi <kayodeolusi@gmail.com>
2022-05-04 11:35:00 +0100

    created the modal

--- components/Story.tsx
+++ components/Story.tsx
@@ -1,20 +1,36 @@
+import React, { useState } from 'react'
+import StoryModal from './StoryModal'
+
 interface Props {
   img: string
   username: string
 }
 
 const Story: React.FC<Props> = ({ img, username }) => {
+  const [showModal, setShowModal] = useState<boolean>(false)
+
   return (
-    <div>
-      <img
-        className="object contain h-14 w-14 cursor-pointer
+    <React.Fragment>
+      {showModal && (
+        <StoryModal
+          setShowModal={setShowModal}
+          image={img}
+          userName={username}
+        />
+      )}
+      <div>
+        <img
+          className="object contain h-14 w-14 cursor-pointer
          rounded-full border-2 border-red-500 p-[1.5px]
          transition duration-200 ease-out hover:scale-110"
-        src={img}
-        alt=""
-      />
-      <p className="w-14 transform truncate text-center text-xs">{username}</p>
-    </div>
+          src={img}
+          alt=""
+        />
+        <p className="w-14 transform truncate text-center text-xs">
+          {username}
+        </p>
+      </div>
+    </React.Fragment>
   )
 }
 
---
+++ components/StoryModal.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  image: string
+  userName: string
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const StoryModal: React.FC<Props> = ({ image, userName, setShowModal }) => {
+  return <div></div>
+}
+
+export default StoryModal
