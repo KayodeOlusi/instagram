cd67dadc1ea85e84939dca54fa22834a86c6d5c5
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2022-11-11 15:02:01 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
+import { RecoilRoot } from 'recoil'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
