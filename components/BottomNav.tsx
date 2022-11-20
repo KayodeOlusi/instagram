c1ec4b067687126fd9d82472ab576c16e4f2a9f2
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2022-11-20 16:45:02 +0100

    Update BottomNav.tsx

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,14 +1,14 @@
-import { PlusCircleIcon } from '@heroicons/react/outline'
-import { HomeIcon, HeartIcon, CameraIcon } from '@heroicons/react/solid'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import { useRecoilState } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
+import { PlusCircleIcon } from '@heroicons/react/outline';
+import { HomeIcon, HeartIcon, CameraIcon } from '@heroicons/react/solid';
 
 const BottomNav: React.FC = () => {
-  const router = useRouter()
-  const { data: session, status } = useSession()
-  const [isOpen, setIsOpen] = useRecoilState(modalState)
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
 
   return (
     <div className="fixed bottom-0 z-50 flex w-full items-center justify-between bg-white p-2 md:hidden">
